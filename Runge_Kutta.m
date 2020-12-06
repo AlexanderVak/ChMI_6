a08efa1fb77a319c2676ba15dc0f8d0a9b445413
AlexanderVak <45165843+AlexanderVak@users.noreply.github.com>
2020-12-07 00:21:00 +0200

    Runge - Kutta Method (Matlab)

---
+++ Runge_Kutta.m
@@ -0,0 +1,17 @@
+f = @(x, y) y + 3*x - x^2;
+t = 0.0;
+h = 0.1;
+y = 1.0;
+last_value = 5;
+
+while t + h <= last_value
+    k1 = h * f(t, y);
+    k2 = h * f(t + 0.5 * h, y + 0.5 * k1);
+    k3 = h * f(t + 0.5 * h, y + 0.5 * k2);
+    k4 = h * f(t + h, y + k3);
+    
+    y = y + (1.0/6.0) * (k1 + 2 * k2 + 2 * k3 + k4);
+    t = t + h;
+    result = y
+end
+

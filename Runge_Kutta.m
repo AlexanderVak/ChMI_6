03a576114c04327b40e5fe9262bb41da4222a73b
AlexanderVak <45165843+AlexanderVak@users.noreply.github.com>
2020-12-07 20:34:29 +0200

    Runge-Kutta plot trouble

--- Runge_Kutta.m
+++ Runge_Kutta.m
@@ -1,8 +1,9 @@
-f = @(x, y) y + 3*x - x^2;
+f = @(x, y) y + 3*x - x.^2;
 t = 0.0;
 y = 1.0;
 last_value = 5;
-
+y_ = double.empty(50,0);
+y_ = [y_,y];
 while t + h <= last_value
     k1 = h * f(t, y);
     k2 = h * f(t + 0.5 * h, y + 0.5 * k1);
@@ -11,6 +12,11 @@ while t + h <= last_value
     
     y = y + (1.0/6.0) * (k1 + 2 * k2 + 2 * k3 + k4);
     t = t + h;
+    y_ = [y_,y];
     result = y
 end
-
+x = 0:h:5;
+f_ = 2*eps.^x + x.^2 - x - 1;
+plot(x, f(x, y_))
+hold on
+plot(x, f_,'r')
